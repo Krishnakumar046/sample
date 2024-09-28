cc9b582726cc8227b9cc484d87400d28c307e308
Krishnakumar046 <krishnakumar6046@gmail.com>
2024-09-28 18:58:42 +0530

    data of use state changes

--- Reactproject/src/component/Usestate.jsx
+++ Reactproject/src/component/Usestate.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 
 export default function Usestate() {
     const [count, setCount] = useState(0);
+
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'increment':
+                return state + 1;
+            case 'decrement':
+                return state - 1
+            default:
+                return state
+        }
+    }
+    const [state, dispatch] = useReducer(reducer, count);
+
+
     return (
         <div>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <p>Count: {state}</p>
+            <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>decrement</button>
+
         </div>
     )
 }
