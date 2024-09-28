0fb38ce087c889b1c03ee1bce4d66c8d2dde044b
Krishnakumar046 <krishnakumar6046@gmail.com>
2024-09-28 19:53:12 +0530

    created navbar

--- Reactproject/src/App.jsx
+++ Reactproject/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Usestate from './component/Usestate'
+import Navbar from './component/navbar'
 
 export default function App() {
   return (
     <div>
+      <Navbar />
       <Usestate />
     </div>
   )
